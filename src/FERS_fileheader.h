ebb8dfe4e563a3616d83f832287bd75abaeac4bd
Genie Jhang <changj@frib.msu.edu>
2024-06-11 14:31:46 +0900

    Updating header data format

--- src/FERS_fileheader.h
+++ src/FERS_fileheader.h
@@ -25,6 +25,8 @@ typedef struct __attribute__((__packed__)) {
 	uint8_t software_minor;
 	uint8_t software_patch;
 	uint8_t acqmode;
+	uint16_t boardtype;
+	uint16_t runnumber;
 	uint16_t energyNCH;
 	uint8_t timeunit;
 	float timeconversion;
