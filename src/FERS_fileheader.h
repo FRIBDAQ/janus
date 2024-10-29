39d83f0b7eb3c9e075961ef44433a67dcfe064af
Genie Jhang <geniejhang@majimak.com>
2024-10-28 23:37:41 -0400

    Correcting the data order

--- src/FERS_fileheader.h
+++ src/FERS_fileheader.h
@@ -24,9 +24,9 @@ typedef struct __attribute__((__packed__)) {
 	uint8_t software_major;
 	uint8_t software_minor;
 	uint8_t software_patch;
-	uint8_t acqmode;
 	uint16_t boardtype;
 	uint16_t runnumber;
+	uint8_t acqmode;
 	uint16_t energyNCH;
 	uint8_t timeunit;
 	float timeconversion;
